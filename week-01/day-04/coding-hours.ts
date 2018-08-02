ad496bf8becfee8eac3a353ab126d519fbf8bb62
Rudolf Grill <grillrudolf@gmail.com>
2018-08-02 16:00:20 +0200

    Coding-hours Done

---
+++ week-01/day-04/coding-hours.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+let dailyCodingHours = 6;
+let semesterLongWeeks = 17;
+let workdays = 5
+let averageWorkHoursWeekly = 52;
+
+console.log(dailyCodingHours * semesterLongWeeks * workdays);
+
+
+
+
---
+++ week-01/day-04/two-numbers.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+// Print the result of 13 added to 22
+// Print the result of 13 substracted from 22
+// Print the result of 22 multiplied by 13
+// Print the result of 22 divided by 13 (as a decimal fraction)
+// Print the reminder of 22 divided by 13
+
+var myNumberA = 13
+var myNumberB = 22
+
+console.log(myNumberA + myNumberB);
+
+console.log(myNumberA - myNumberB);
+
+console.log(myNumberB * myNumberA);
+
+console.log(Math.round (myNumberA / myNumberB));
+
+console.log(myNumberB % myNumberA);
