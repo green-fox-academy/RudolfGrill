f5a6aaf9aa7ae87321f3a622202d5e327898c430
Rudolf Grill <grillrudolf@gmail.com>
2018-08-02 23:28:39 +0200

    next is '14 - conditional-variable-mutation'

--- week-01/day-04/cuboid.ts
+++ week-01/day-04/cuboid.ts
@@ -6,3 +6,12 @@
 // Surface Area: 600
 // Volume: 1000
 
+let cuboidLength: number = 10;
+let cuboidWidth: number = 4;
+let cuboidHeight: number = 5;
+
+let SurfaceArea: number = 2 * (cuboidHeight * cuboidLength + cuboidHeight * cuboidWidth + cuboidLength * cuboidWidth);
+console.log(SurfaceArea);
+
+let volumeCuboid: number = cuboidHeight * cuboidLength * cuboidWidth;
+console.log(volumeCuboid);
--- week-01/day-04/variable-mutation.ts
+++ week-01/day-04/variable-mutation.ts
@@ -23,18 +23,29 @@ console.log(e**3);
 let f1: number = 123;
 let f2: number = 345;
 // tell if f1 is bigger than f2 (as a boolean)
-
+console.log(f1 > f2);
 
 let g1: number = 350;
 let g2: number = 200;
 // tell if the double of g2 is bigger than g1 (pras a boolean)
+console.log(g1 < (g2*2));
+
+
 let h: number = 1357988018575474;
+let zero: number = 0;
 // tell if h has 11 as a divisor (as a boolean)
+console.log((h % 11)=== zero);
+
 let i1: number = 10;
 let i2: number = 3;
 // tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
+console.log(i1 > i2**2 && i1 < i2**3);
+
 let j: number = 1521;
 // tell if j is dividable by 3 or 5 (as a boolean)
+console.log(j % 3 == 0 || j % 5 == 0);
+
 let k: string = 'Apple';
 // fill the k variable with its content 4 times
+k = k + k + k + k;
 console.log(k);
