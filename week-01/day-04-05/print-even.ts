1ed34b99cb6aadc44f82e9aecaf1708c5297caa9
Rudolf Grill <grillrudolf@gmail.com>
2018-08-03 11:52:01 +0200

    Math done

--- week-01/day-04-05/print-even.ts
+++ week-01/day-04-05/print-even.ts
@@ -5,6 +5,6 @@
 let evennumbers: number = 0;
 while (evennumbers < 501) {
   if (evennumbers % 2 ==0)
-    console.log(evennumbers); // Prints the numbers from 0 to 9
+    console.log(evennumbers);
   evennumbers++;
 }
