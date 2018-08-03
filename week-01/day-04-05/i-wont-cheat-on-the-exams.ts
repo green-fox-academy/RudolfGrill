abfdd7f594e8673b1200c242858a8bbee7aeb590
Rudolf Grill <grillrudolf@gmail.com>
2018-08-03 10:47:10 +0200

    i-wont-cheat-on-the-exams is done

---
+++ week-01/day-04-05/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,9 @@
+
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (let print100time: number = 0; print100time < 100; print100time++) {
+    console.log('I won\'t cheat on the exam!'); // Prints the numbers from 0 to 99
+  }
