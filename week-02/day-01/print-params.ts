198ee158a078c613fe769da3c5984766f0ef5aed
Rudolf Grill <grillrudolf@gmail.com>
2018-08-06 17:40:44 +0200

    done until compare-length

---
+++ week-02/day-01/print-params.ts
@@ -0,0 +1,16 @@
+'use strict';
+export{}
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+
+function printParams(...anything: any[]){
+    anything.forEach(function(anything: any){
+    console.log(anything);
+    });
+}
+
+printParams("Bill", "Joe", "Smith", 17, 23);
+printParams("pear", "banana", "apple", "orange");
