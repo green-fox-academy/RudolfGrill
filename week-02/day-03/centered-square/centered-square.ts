dcbed5e5fdbbe237b3dab7bc65b0d421d8728b7b
Rudolf Grill <grillrudolf@gmail.com>
2018-08-08 16:03:57 +0200

    s

--- week-02/day-03/centered-square/centered-square.ts
+++ week-02/day-03/centered-square/centered-square.ts
@@ -1,13 +1,17 @@
+ 
 'use strict';
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 // Draw a green 10x10 square to the center of the canvas.
 
+let x:number = 10;
 
 ctx.fillStyle = 'green';
-ctx.fillRect(canvas.width / 2, canvas.height / 2, 10, 10);
+
+ctx.fillRect(((canvas.width / 2)-x), ((canvas.height / 2)-x), x, x);
 
 
