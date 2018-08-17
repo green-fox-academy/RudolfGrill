ae88218831b37f0c673ca5126f895cd5feb4d298
RudolfGrill <grillrudolf@gmail.com>
2018-08-17 16:46:09 +0200

    feat:create a good tria.

--- week-03/day-05/myFractal1/myFractal1.ts
+++ week-03/day-05/myFractal1/myFractal1.ts
@@ -3,9 +3,19 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+ctx.fillStyle = getRandomColor();
+
+function getRandomColor() {
+  let letters: string = '0123456789ABCDEF';
+  let color: string = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function drawSquares(xCor: number, yCor: number, size: number, loop: number) {
-  if (loop > 0) {
+  if (loop > 5) {
     ctx.strokeRect(xCor + size, yCor, size, size);
     ctx.strokeRect(xCor, yCor + size, size, size);
     ctx.strokeRect(xCor + 2 * size, yCor + size, size, size);
@@ -13,9 +23,16 @@ function drawSquares(xCor: number, yCor: number, size: number, loop: number) {
 
     let B: number = 3
     let newsize = size / B;
+    ctx.strokeStyle = getRandomColor();
     drawSquares(xCor + B * newsize, yCor, newsize, loop - 1);
+    ctx.strokeStyle = getRandomColor();
+
     drawSquares(xCor, yCor + B * newsize, newsize, loop - 1);
+    ctx.strokeStyle = getRandomColor();
+
     drawSquares(xCor + 2 * B * newsize, yCor + B * newsize, newsize, loop - 1);
+    ctx.strokeStyle = getRandomColor();
+
     drawSquares(xCor + B * newsize, yCor + 2 * B * newsize, newsize, loop - 1);
 
 
