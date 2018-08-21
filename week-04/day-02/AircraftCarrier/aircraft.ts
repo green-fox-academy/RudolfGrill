753ea9c1a86db4e8a539957b1424cd53e67e67cf
RudolfGrill <grillrudolf@gmail.com>
2018-08-21 17:02:39 +0200

    aircraft get a new feat

--- week-04/day-02/AircraftCarrier/aircraft.ts
+++ week-04/day-02/AircraftCarrier/aircraft.ts
@@ -1,12 +1,22 @@
 'use strict';
 
-class Aircraft{
-   fight(){
-     
-   }
+class Aircraft {
+  private typeOfAircraft: string;
+  private ammo: number;
+  private damage: number;
+  constructor(typeOfAircraft: string, ammo: number = 0, damage: number) {
+    this.typeOfAircraft = typeOfAircraft;
+    this.ammo = ammo;
+    this.damage = damage;
+  }
+  fight() {
+//fight
+//It should use all the ammos (set it to 0) and it should return the damage it deals
+//The damage is the multiplication of the base damage and the ammos
+  }
 }
 
-class F16 {
+export class F16 {
   private ammo: number;
   private damage: number;
 
@@ -18,12 +28,18 @@ class F16 {
 
 }
 
-class F35 {
+export class F35 {
   private ammo: number;
   private damage: number;
 
   constructor(ammo: number = 0, damage: number = 50) {
-    this. ammo= ammo;
+    this.ammo = ammo;
     this.damage = damage;
   }
 }
+
+let F16_1 = new F16;
+let F35_1 = new F35;
+
+console.log(F35_1);
+console.log(F16_1);
