f827659b87e267e7372d7595135b99a23ad306ab
RudolfGrill <grillrudolf@gmail.com>
2018-09-20 09:28:36 +0200

    tables ok

--- week-07/day-03/bookstore/assets/js/script.js
+++ week-07/day-03/bookstore/assets/js/script.js
@@ -3,7 +3,7 @@
 window.onload = () => {
   const host = 'http://localhost:8080/api';
   const myRequest = new XMLHttpRequest();
-  const bookParent = document.querySelector('#bookTableRow');
+  const bookParent = document.querySelector('#bookContainer');
 
   myRequest.open('GET', `${host}/books`, true);
 
@@ -12,17 +12,28 @@ window.onload = () => {
   myRequest.onload = () => {
     if (myRequest.status === 200) {
       const bookList = JSON.parse(myRequest.response);
-      console.log(bookList);
       bookList.forEach(book => {
-        let newTitle = document.createElement('th');
-        bookParent.appendChild(newTitle);
+        let newRow = document.createElement('tr');
+        bookParent.appendChild(newRow);
+        let newTitle = document.createElement('td');
+        newRow.appendChild(newTitle);
         newTitle.innerText = book.book_name;
-        let newTitle = document.createElement('th');
-        bookParent.appendChild(newTitle);
-        newTitle.innerText = book.aut_name;
+        let newAuthor = document.createElement('td');
+        newRow.appendChild(newAuthor);
+        newAuthor.innerText = book.aut_name;
+        let newCategory = document.createElement('td');
+        newRow.appendChild(newCategory);
+        newCategory.innerText = book.cate_descrip;
+        let newPublisher = document.createElement('td');
+        newRow.appendChild(newPublisher);
+        newPublisher.innerText = book.pub_name
+        let newPrice = document.createElement('td');
+        newRow.appendChild(newPrice);
+        newPrice.innerText = book.book_price;
       });
     }
   }
-  myRequest.send();
-}
 
+  myRequest.send();
+
+}
