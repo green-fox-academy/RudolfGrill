683f879240024b8fae9c8305071c801921c502d3
RudolfGrill <grillrudolf@gmail.com>
2018-09-27 20:09:00 +0200

    (status) app is working ok

--- week-08/day-04/buttonDisable/index.js
+++ week-08/day-04/buttonDisable/index.js
@@ -1,13 +1,31 @@
-'use strict';
+'use strict'
 
 window.onload = () => {
-  const favouritePet = document.querySelector('#signUp');
-  const catLover = document.querySelector('#loveCats');
+  const favouritePetButton = document.querySelector("button[type=submit]");
+  const catLoverButton = document.querySelector("button[type=loveCats]");
+  const favouritePetDiv = document.querySelector("#favouritePetDiv");
+  const catFactDiv = document.querySelector("#catFactDiv");
 
-  console.log(favouritePet);
-  //console.log(catLover);
+  //console.log(favouritePetButton);
+  //console.log(catLoverButton);
+  //console.log(favouritePetDiv);
+  //console.log(catFactDiv);
 
-  favouritePet.addEventListener('signUp', (event) => {
-    console.log(event);
-  });
+  favouritePetDiv.addEventListener('change', (event) => {
+    if (event.target.value !== 'goldfish') {
+      favouritePetButton.disabled = false;
+    } else {
+      favouritePetButton.disabled = true;
+    }
+  })
+  
+  catFactDiv.addEventListener('change', (event) => {
+    if (event.target.value === 'yes') {
+      catLoverButton.disabled = false;
+      alert("Thank you, you've successfully signed up for cat facts")
+    } else if (event.target.value === 'no'){
+      catLoverButton.disabled = true;
+      alert("Sigh, we still added you to the cat facts list");
+    }
+  })
 };
