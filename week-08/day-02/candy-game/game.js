a2118298ce38846666d76718732240b15e3f8389
RudolfGrill <grillrudolf@gmail.com>
2018-10-01 16:57:48 +0200

    (feat)queryselectors added

---
+++ week-08/day-02/candy-game/game.js
@@ -0,0 +1,23 @@
+const candiesNumber = document.querySelector('.candies');
+const lollypopsNumber = document.querySelector('.lollypops');
+const speedNum = document.querySelector('.speed');
+console.log(candiesNumber);
+console.log(lollypopsNumber);
+console.log(speedNum);
+
+const button = document.querySelector('button');
+
+const createCandiesButton = document.querySelector('.create-candies');
+const buyLollypopsButton = document.querySelector('.buy-lollypops');
+const candyMachineButton = document.querySelector('.candy-machine');
+console.log(createCandiesButton);
+console.log(buyLollypopsButton);
+console.log(candyMachineButton);
+
+button.addEventListener('click', (event)=>{
+
+
+
+
+
+})
