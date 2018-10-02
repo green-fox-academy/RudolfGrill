bd223d81d68614c0ae61127014686c61c42c0488
RudolfGrill <grillrudolf@gmail.com>
2018-10-02 10:01:24 +0200

    (feat)upvote,downvote button added

--- week-07/reddit/assets/js/script.js
+++ week-07/reddit/assets/js/script.js
@@ -53,23 +53,30 @@ window.onload = () => {
         modify.classList.add('modify');
         modify.innerText = 'MODIFY';
 
-        let upvote = document.createElement('img');
-        scorediv.appendChild(upvote);
-        upvote.classList.add('upvote');
-        upvote.setAttribute("src", "./assets/css/imgs/upvote.png");
+        let upvotebtn = document.createElement('button');
+        scorediv.appendChild(upvotebtn);
+        upvotebtn.classList.add('upvotebtn');
+
+        let upvoteimg = document.createElement('img');
+        upvotebtn.appendChild(upvoteimg);
+        upvoteimg.classList.add('upvoteimg');
+        upvoteimg.setAttribute("src", "./assets/css/imgs/upvote.png");
 
         let score = document.createElement('div');
         scorediv.appendChild(score);
         score.classList.add('score');
         score.innerText = element.score;
 
-        let downvote = document.createElement('img');
-        scorediv.appendChild(downvote);
-        downvote.classList.add('downvote');
-        downvote.setAttribute("src", "./assets/css/imgs/downvote.png");
+        let downvotebtn = document.createElement('button');
+        scorediv.appendChild(downvotebtn);
+        downvotebtn.classList.add('downvotebtn');
+
+        let downvoteimg = document.createElement('img');
+        downvotebtn.appendChild(downvoteimg);
+        downvoteimg.classList.add('downvoteimg');
+        downvoteimg.setAttribute("src", "./assets/css/imgs/downvote.png");
 
         
-
       })
     }
   }
