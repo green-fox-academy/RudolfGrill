9b7b8739f57e7fac78f7846add33187cc94938ab
RudolfGrill <grillrudolf@gmail.com>
2018-10-04 15:10:47 +0200

    (feat)html rendering

--- week-09/day-04/quizApplication/server.js
+++ week-09/day-04/quizApplication/server.js
@@ -32,19 +32,41 @@ app.get('/', (req, res) => {
 });
 
 app.get('/game', (req, res) => {
-    conn.query(`SELECT questions.id as question_id, question, answers.id, answer, is_correct FROM questions INNER JOIN answers ON questions.id = answers.question_id`, (error, result) => {
-    if (error) {
-      console.log('Error connecting to database', error.message);
-      res.status(500).send('Database error');
+  const randomquestion = `SELECT * FROM questions ORDER BY RAND() LIMIT 1;`;
+  conn.query(randomquestion, (err, result) => {
+    if (err) {
+      console.log(err.toString());
+      return;
+    } else {
+      let questionID = result[0].id;
+      let question = result[0].question;
+      console.log(questionID, question);
+      const answerquery = `select answers.id, question_id, answer, is_correct from answers INNER JOIN questions ON answers.question_id=questions.id where question_id = ${questionID};`;
+      conn.query(answerquery, (err, result) => {
+        if (err) {
+          console.log(err.toString());
+          return;
+        }
+        console.log(result)
+        res.json({
+          questionId: questionID,
+          question: question,
+          answers: result
+        })
+      })
+    }
+  })
+})
+
+app.get('/questions', (req, res) => { //This endpoint should return all the questions.
+  const questionlistquery = `SELECT questions.id, question FROM questions;`
+  conn.query(questionlistquery, (err, result) => {
+    if (err) {
+      console.log(err.toString());
       return;
     }
-    res.status(200).send(result)
-    })
-  
-});
-
-app.get('/questions', (req, res) => {
-
+    res.json(result)
+  })
 });
 
 app.post('/questions', (req, res) => {
@@ -54,12 +76,6 @@ app.post('/questions', (req, res) => {
 app.delete('/questions/id', (res, req) => {
 
 });
-
-
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
